d18cae4c47392dbbfd4583a1df605d2bed14f181
Muaykhao86 <waveflow18@gmail.com>
2019-09-19 17:02:40 +0100

    first push

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Form from './Components/Form'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Form/>
+      To Log out del cookie
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Form.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react'
+
+export default class Form extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            email: '',
+            password: '',
+        }
+
+    }
+
+    saveToState = (e) => {
+        this.setState({[e.target.name]:[e.target.value]});
+    }
+
+    SendForm = () => {
+        fetch('https://www.eurobrake.net/authors/login', {
+            method: 'post',
+            mode: 'no-cors',
+            credentials: 'include',
+            headers: {
+            'Content-Type': 'text/plain'
+            },
+            body: {
+                username: this.state.email,
+                password: this.state.password
+            }
+        }).then(
+            res => console.table(res)
+        )
+    }
+
+    render() {
+        return (
+            <div>
+                <label htmlFor="">
+                    Email
+     <input type="email" name="email" id="" onChange={this.saveToState}/>
+                </label>
+                <label htmlFor="">
+                    Password
+     <input type="password" name="password" id="" onChange={this.saveToState}/>
+                </label>
+                <label htmlFor="">Login
+     <input type="submit" value="Login" onClick={this.SendForm}/>               
+                </label>
+                <label htmlFor="">Logout
+     <input type="submit" value="Logout" onClick={this.LogOut}/>               
+                </label>
+            </div>
+        )
+    }
+}
+
