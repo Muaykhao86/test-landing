98a74703ff9a1478ccdfb2abe05131d997b7ead1
Muaykhao86 <waveflow18@gmail.com>
2019-09-19 16:49:42 +0100

    Updates

---
+++ precache-manifest.d482e38a11bbe0fda4f2787ddeef10ff.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44bdda9e410fd4caec58ce0720ca2bb7",
+    "url": "/,github.io/test-landing/index.html"
+  },
+  {
+    "revision": "4bead192487dd2a08a74",
+    "url": "/,github.io/test-landing/static/css/main.6681a07a.chunk.css"
+  },
+  {
+    "revision": "06dd1366ad51164c447d",
+    "url": "/,github.io/test-landing/static/js/2.25495141.chunk.js"
+  },
+  {
+    "revision": "4bead192487dd2a08a74",
+    "url": "/,github.io/test-landing/static/js/main.ee018db7.chunk.js"
+  },
+  {
+    "revision": "2b29470e3dd2d6f0fff1",
+    "url": "/,github.io/test-landing/static/js/runtime~main.efd780ee.js"
+  }
+]);
